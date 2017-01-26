680ede42eb1d14c00bb0dbf28029cf3cc1bba2c0
wert <wert@thinserver17a.cla>
2017-01-26 11:37:12 +0100

    view password admin action :)

--- admin.py
+++ admin.py
@@ -4,6 +4,7 @@ from .models import *
 from admin_inline_fk import *
 
 from django.contrib import admin
+from django.contrib import messages
 
 class EncodingFormatAdmin(admin.ModelAdmin):
     #search_fields = ['codice_stazione_regione']
@@ -21,7 +22,14 @@ class ServerAdmin(admin.ModelAdmin):
 admin.site.register(Server, ServerAdmin)
 
 class PasswordVaultAdmin(admin.ModelAdmin):
-    list_display  = [ 'username', 'password', 'servers', 'hashes', 'is_active'  ]
+    def view_password(modeladmin, request, queryset):      
+        from pVault.models import PasswordVault
+        for i in queryset:
+            pv = PasswordVault.objects.get(username=i.username).view()
+            messages.add_message(request, messages.INFO, '%s password is: %s' % (i.username, pv))
+    view_password.short_description = "decrypt password and print it on screen"
+    
+    list_display  = [ 'username', 'servers', 'hashes', 'is_active'  ]
     inlines       = [ PasswordVaultExportsInLine, PasswordVaultHistoryInLine ]
-
+    actions       = [view_password,]
 admin.site.register(PasswordVault, PasswordVaultAdmin)
